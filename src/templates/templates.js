0f1057f4318f589e730d111cf4e8be06d6e5f24d
Christian McHugh <mchughc@dnb.com>
2018-02-16 06:32:20 -0800

    Add event links

--- src/templates/templates.js
+++ src/templates/templates.js
@@ -14,7 +14,7 @@
     function populateItems(triggerData) {
       var config = $.getLocalConfig();
       var buffer = "";
-      
+
       buffer += `<tr class="` + escapeExpression(triggerData.priorityClass) + `"><td class="system">`;
       buffer += `<i class="arrow" title="arrow icon"></i>` + escapeExpression(triggerData.host)
       buffer += `
@@ -23,12 +23,14 @@
       buffer += escapeExpression(triggerData.name) + `</td><td class="priority">`;
       buffer += escapeExpression(triggerData.priority) + `</td><td class="age">`;
       buffer += escapeExpression(triggerData.age) + "</td></tr>";
-      buffer += `<tr id="advanced" class="hidden"><td id="advanced" colspan="4">
-      <a href="` + config.zabbixBase + `hostinventories.php?hostid=` + triggerData.hostid + `" class="button" id="zablink" target="_blank">Host Details</a>
-      <a href="` + config.zabbixBase + `latest.php?fullscreen=0&filter_set=1&show_without_data=1&hostids%5B%5D=` + triggerData.hostid + `" class="button" id="zablink" target="_blank">Latest Data</a>
-      <a href="` + config.zabbixBase + `charts.php?fullscreen=0&groupid=0&graphid=0&hostid=` + triggerData.hostid + `" class="button" id="zablink" target="_blank">Host Graphs</a>
+      buffer += `<tr id="advanced" class="hidden"><td id="` + escapeExpression(triggerData.triggerid) + `" colspan="4">
+      <a href="` + config.zabbixBase + `hostinventories.php?hostid=` + triggerData.hostid + `" class="button zablink" id="zablink" target="_blank">Host Details</a>
+      <a href="` + config.zabbixBase + `latest.php?fullscreen=0&filter_set=1&show_without_data=1&hostids%5B%5D=` + triggerData.hostid + `" class="button zablink" id="zablink" target="_blank">Latest Data</a>
+      <a href="` + config.zabbixBase + `charts.php?fullscreen=0&groupid=0&graphid=0&hostid=` + triggerData.hostid + `" class="button zablink" id="zablink" target="_blank">Host Graphs</a>
        | 
-      <a href="` + config.zabbixBase + `zabbix.php?action=problem.view&filter_set=1&filter_triggerids%5B%5D=` + triggerData.triggerid + `" class="button" id="zablink" target="_blank">Problem Details</a>
+      <a href="` + config.zabbixBase + `zabbix.php?action=problem.view&filter_set=1&filter_triggerids%5B%5D=` + triggerData.triggerid + `" class="button zablink" id="zablink" target="_blank">Problem Details</a>
+      <a class="button zablink" id="eventLink">Event Details</a>
+      <a class="button zablink" id="eventAck">Ack Event</a>
       </td></tr>`;
       return buffer;
     }
